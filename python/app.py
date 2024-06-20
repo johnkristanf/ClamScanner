807a48c4545e61a08d58ca66f912a093505c0e66
John Kristan <jkpogz09@gmail.com>
2024-06-20 16:24:22 +0800

    Added the new uploading images and dataset classes endpoint

--- python/app.py
+++ python/app.py
@@ -8,6 +8,9 @@ from flask_cors import CORS
 from ws_client import clients
 from fastapi.middleware.cors import CORSMiddleware
 
+from ds.ds_ops import add_new_dataset_class, delete_dataset_class
+from ds.upload_ds_image import process_image_uploading, DB_DATASETS, count_images
+
 from train.process_train import train_new_model
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
@@ -28,7 +31,13 @@ ws_app.add_middleware(
 )
 
 
-DATASET_FOLDER = '../datasets'
+DATASET_FOLDER = os.path.abspath('./datasets')
+
+def dataset_db_update():
+    global update_dataset_class_data 
+    update_dataset_class_data = DB_DATASETS(app)
+
+dataset_db_update()
 
 
 @ws_app.websocket("/ws")
@@ -52,12 +61,7 @@ async def websocket_endpoint(websocket: WebSocket):
 @app.post("/image/scan")
 def scan():
     try:
-        if 'captured_image_file' not in request.files:
-            return jsonify({'error': 'No file part in the request'}), 400
-
-        file = request.files['captured_image_file']
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
+        file = predict.load_validate_image_file(request, jsonify)
 
         filename = secure_filename(file.filename)
         file_path = os.path.join('./', filename)
@@ -89,7 +93,7 @@ def train_model_async(data):
         "validation_loss": val_loss
     }
 
-    return 'AYDULLL PA PIKSHURR'
+    return 'Trained Successfully'
 
 
 @app.post("/train/model")
@@ -99,11 +103,53 @@ def train():
     return jsonify({"status": "Training started"})
 
 
+@app.post("/add/dataset/class")
+def add_dataset():
+    data = request.get_json()
+    
+    folder_path = data['folder_path']
+
+    add_new_dataset_class(folder_path)
+    return jsonify({"success": "Dataset Class Added!"}), 200
+
+
+
+@app.post("/upload/dataset/images")
+def upload_images():
+    dataset_class = request.form.get('datasetClass')
+    class_id = request.form.get('class_id')
+
+    if 'images' not in request.files:
+        return jsonify({"error": "No files part in the request"}), 400
+
+    images = request.files.getlist('images')
+    dest_folder = os.path.join("datasets", dataset_class)
+
+    process_image_uploading(images, dest_folder)
+    img_count = count_images(dest_folder)
+    
+    update_dataset_class_data(img_count, class_id)
+
+    return 'Image Uploaded Successfull', 200
+
+
+
+@app.post("/delete/dataset/class")
+def delete_dataset():
+    data = request.get_json()
+    
+    folder_path = data['folder_path']
+
+    delete_dataset_class(folder_path)
+    return jsonify({"success": "Dataset Class Added!"}), 200
+
+
 def run_flask():
     app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
 
 def run_fastapi():
     uvicorn.run(ws_app, port=8000)
+
 
 if __name__ == '__main__':
     flask_thread = threading.Thread(target=run_flask)
