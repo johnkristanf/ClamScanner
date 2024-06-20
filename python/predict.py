1c3aff9f4c76531025a6130d418324b1081e183e
John Kristan <jkpogz09@gmail.com>
2024-06-20 16:25:00 +0800

    changes the loading of dataset classes to folder name base

--- python/predict.py
+++ python/predict.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+
 from PIL import Image
 from keras._tf_keras.keras.applications.resnet import preprocess_input
 from keras._tf_keras.keras.saving import load_model
@@ -9,11 +10,9 @@ print("WORKING DIR:", os.getcwd())
 
 model_path = os.path.abspath("./models/ClamScanner_best_v2.keras")
 
-def load_model_once():
+def load_resnet_model():
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"Model file does not exist: {model_path}")
-    else:
-        print("GA EXIST ANG FILE DOLLL NAA SA:", model_path)
 
     global resnet_model
     resnet_model = load_model(model_path)
@@ -22,7 +21,14 @@ def load_model_once():
     # resnet_model.compile(optimizer=Adam(learning_rate=1e-4), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     # print("Model recompiled successfully.")
 
-load_model_once()
+
+load_resnet_model()
+
+def load_dataset_classes():
+    global CLASSES 
+    CLASSES = [file.name for file in os.scandir("datasets") if file.is_dir()]
+
+load_dataset_classes()
 
 
 def resize_and_preprocess_image(image_path):
@@ -39,9 +45,21 @@ def mollusk_predict(image_path):
 
     print("predictions", predictions)
 
-    CLASSES = ['Blood Clam', 'BullMouth Helmet', 'Horn Snail', 'Invalid Image', 'Mussel', 'Oyster', 'Scaly Clam', 'Tiger Cowrie']
+    # CLASSES = ['Blood Clam', 'BullMouth Helmet', 'Horn Snail', 'Invalid Image', 'Mussel', 'Oyster', 'Scaly Clam', 'Tiger Cowrie']
     print("CLASSES", CLASSES)
 
     mollusk_classified_result = CLASSES[np.argmax(predictions)]
     return mollusk_classified_result
 
+
+def load_validate_image_file(request, jsonify):
+
+    if 'captured_image_file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['captured_image_file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    return file
+    
