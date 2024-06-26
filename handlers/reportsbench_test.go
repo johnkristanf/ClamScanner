921c858c7ba6db5d93ebf943fa2eca67ada8c4f5
John Kristan <jkpogz09@gmail.com>
2024-06-26 17:20:47 +0800

    IMPLEMENTED the benchmark testing of update and delete report handlers

--- handlers/reportsbench_test.go
+++ handlers/reportsbench_test.go
@@ -106,3 +106,143 @@ func BenchmarkFetchAllReportsHandler(b *testing.B) {
 	mockReportsJSON.AssertExpectations(b)
 
 }
+
+
+func BenchmarkFetchYearlyReportByCityHandler(b *testing.B) {
+	fetchCityReports := []*types.YearlyReportsPerCity{
+		{
+			City: "Panabo",
+			Year: "2024",
+			ReportsCount: 10,
+		},
+
+		{
+			City: "Tagum",
+			Year: "2024",
+			ReportsCount: 5,
+		},
+	}
+	mockReportsDB.On("FetchPerCityReports").Return(fetchCityReports, nil)
+	mockRedis.On("GET",  mock.Anything,  mock.Anything, mock.Anything).Return(redis.Nil)
+	mockRedis.On("SET",  mock.Anything,  mock.Anything, mock.Anything).Return(nil)
+	mockReportsJSON.On("JsonEncode", mock.Anything, http.StatusOK, fetchCityReports).Return(nil)
+
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest("GET", "/fetch/reports/city", nil)
+		rr := httptest.NewRecorder()
+
+		benchmarkReportsHandlers.FetchYearlyReportByCityHandler(rr, req)
+	}
+
+	mockReportsDB.AssertExpectations(b)
+	mockRedis.AssertExpectations(b)
+	mockReportsJSON.AssertExpectations(b)
+}
+
+
+
+
+func BenchmarkFetchYearlyReportByProvinceHandler(b *testing.B) {
+
+	fetchProvinceReports := []*types.YearlyReportsPerProvince{
+		{
+			Province: "Davao Del Norte",
+			Year: "2024",
+			ReportsCount: 10,
+		},
+	
+		{
+			Province: "Davao Del Norte",
+			Year: "2024",
+			ReportsCount: 5,
+		},
+	}
+
+	mockReportsDB.On("FetchPerProvinceReports").Return(fetchProvinceReports, nil)
+	mockRedis.On("GET",  mock.Anything,  mock.Anything, mock.Anything).Return(redis.Nil)
+	mockRedis.On("SET",  mock.Anything,  mock.Anything, mock.Anything).Return(nil)
+	mockReportsJSON.On("JsonEncode", mock.Anything, http.StatusOK, fetchProvinceReports).Return(nil)
+
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest("GET", "/fetch/reports/province", nil)
+		rr := httptest.NewRecorder()
+
+		benchmarkReportsHandlers.FetchYearlyReportByProvinceHandler(rr, req)
+	}
+
+	mockReportsDB.AssertExpectations(b)
+	mockRedis.AssertExpectations(b)
+	mockReportsJSON.AssertExpectations(b)
+}
+
+
+func BenchmarkFetchReportPerMollusk(b *testing.B) {
+
+	fetchMollusksReports := []*types.ReportsPerMollusk{
+		{
+			MolluskType: "Scaly Clam",
+			MolluskCount: 1,
+		},
+	
+		{
+			MolluskType: "Tiger Cowrie",
+			MolluskCount: 2,
+		},
+	}
+
+	mockReportsDB.On("FetchReportsPerMollusk").Return(fetchMollusksReports, nil)
+	mockRedis.On("GET",  mock.Anything,  mock.Anything, mock.Anything).Return(redis.Nil)
+	mockRedis.On("SET",  mock.Anything,  mock.Anything, mock.Anything).Return(nil)
+	mockReportsJSON.On("JsonEncode", mock.Anything, http.StatusOK, fetchMollusksReports).Return(nil)
+
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest("GET", "/fetch/reports/mollusk", nil)
+		rr := httptest.NewRecorder()
+
+		benchmarkReportsHandlers.FetchReportPerMollusk(rr, req)
+	}
+
+	mockReportsDB.AssertExpectations(b)
+	mockRedis.AssertExpectations(b)
+	mockReportsJSON.AssertExpectations(b)
+}
+
+func BenchmarkUpdateReportStatusHandler(b *testing.B) {
+
+	mockReportsDB.On("UpdateReportStatus", int64(4)).Return(nil)
+	mockRedis.On("DELETE",  "reports",  mock.Anything).Return(nil)
+	mockReportsJSON.On("JsonEncode", mock.Anything, http.StatusOK, "Report Status Updated!").Return(nil)
+
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest("PUT", "/update/report/status/{report_id}", nil)
+		req.SetPathValue("report_id", "4")
+
+		rr := httptest.NewRecorder()
+
+		benchmarkReportsHandlers.UpdateReportStatusHandler(rr, req)
+	}
+
+	mockReportsDB.AssertExpectations(b)
+	mockRedis.AssertExpectations(b)
+	mockReportsJSON.AssertExpectations(b)
+}
+
+func BenchmarkDeleteReportHandler(b *testing.B) {
+
+	mockReportsDB.On("DeleteReportCases", int64(4)).Return(nil)
+	mockRedis.On("DELETEBYKEY",  reportKeys,  mock.Anything).Return(nil)
+	mockReportsJSON.On("JsonEncode", mock.Anything, http.StatusOK, mock.AnythingOfType("string")).Return(nil)
+
+	for i := 0; i < b.N; i++ {
+		req := httptest.NewRequest("DELETE", "/delete/reports/{report_id}", nil)
+		req.SetPathValue("report_id", "4")
+
+		rr := httptest.NewRecorder()
+
+		benchmarkReportsHandlers.DeleteReportHandler(rr, req)
+	}
+
+	mockReportsDB.AssertExpectations(b)
+	mockRedis.AssertExpectations(b)
+	mockReportsJSON.AssertExpectations(b)
+}
