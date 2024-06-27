97cc3f1b890234405eb408cec1f581244f1feadf
John Kristan <jkpogz09@gmail.com>
2024-06-27 16:56:11 +0800

    added environment var for TF_ENABLE_ONEDNN_OPTS

--- python/app.py
+++ python/app.py
@@ -8,8 +8,8 @@ from flask_cors import CORS
 from ws_client import clients
 from fastapi.middleware.cors import CORSMiddleware
 
-from ds.ds_ops import add_new_dataset_class, delete_dataset_class
-from ds.upload_ds_image import process_image_uploading, DB_DATASETS, count_images
+from upload.ds_ops import add_new_dataset_class, delete_dataset_class
+from upload.upload_ds_image import process_image_uploading, DB_DATASETS, count_images
 
 from train.process_train import train_new_model
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
@@ -30,75 +30,14 @@ ws_app.add_middleware(
     allow_headers=["*"], 
 )
 
-
-DATASET_FOLDER = os.path.abspath('./datasets')
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+DATASET_FOLDER = os.path.abspath('datasets')
 
 def dataset_db_update():
     global update_dataset_class_data 
     update_dataset_class_data = DB_DATASETS(app)
 
 dataset_db_update()
-
-
-@ws_app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    clients.append(websocket)
-    print(f"Client connected: {websocket.client}")
-    try:
-        while True:
-            message = await websocket.receive_text()
-            print(f"Received message: {message}")
-    except WebSocketDisconnect:
-        print(f"Client disconnected: {websocket.client}")
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        clients.remove(websocket)
-        print(f"Client removed: {websocket.client}")
-
-
-@app.post("/image/scan")
-def scan():
-    try:
-        file = predict.load_validate_image_file(request, jsonify)
-
-        filename = secure_filename(file.filename)
-        file_path = os.path.join('./', filename)
-        file.save(file_path)
-        
-        mollusk_classified_result = predict.mollusk_predict(file_path)
-        os.remove(file_path)
-        
-        return jsonify({"mollusk_classified_result": mollusk_classified_result}), 200
-
-    except Exception as e:
-        print("Error during image processing:", e)
-        return jsonify({"error": "Image processing failed"}), 500
-
-
-def train_model_async(data):
-    model_version = data.get('version')
-    print("model_version:", model_version)
-
-    train_acc, val_acc, train_loss, val_loss = train_new_model(DATASET_FOLDER, model_version)
-
-    data = {
-        "version": f'ClamScanner_v{model_version}',
-        "train_accuracy": train_acc,
-        "validation_accuracy": val_acc,
-        "train_loss": train_loss,
-        "validation_loss": val_loss
-    }
-
-    return 'Trained Successfully'
-
-
-@app.post("/train/model")
-def train():
-    data = request.get_json()
-    threading.Thread(target=train_model_async, args=(data,)).start()
-    return jsonify({"status": "Training started"}), 200
 
 
 @app.post("/add/dataset/class")
@@ -121,7 +60,9 @@ def upload_images():
         return jsonify({"error": "No files part in the request"}), 400
 
     images = request.files.getlist('images')
-    dest_folder = os.path.join("./datasets", dataset_class)
+    dest_folder = os.path.join("datasets", dataset_class)
+
+    os.makedirs(dest_folder, exist_ok=True)
 
     process_image_uploading(images, dest_folder)
     img_count = count_images(dest_folder)
@@ -130,10 +71,7 @@ def upload_images():
     update_dataset_class_data(img_count, class_id)
     print("updated successfulyy")
 
-
-
     return 'Image Uploaded Successfull', 200
-
 
 
 @app.post("/delete/dataset/class")
@@ -144,6 +82,66 @@ def delete_dataset():
 
     delete_dataset_class(folder_path)
     return jsonify({"success": "Dataset Class Deleted!"}), 200
+
+
+@ws_app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    print(f"Client connected: {websocket.client}")
+    try:
+        while True:
+            message = await websocket.receive_text()
+            print(f"Received message: {message}")
+    except WebSocketDisconnect:
+        print(f"Client disconnected: {websocket.client}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        clients.remove(websocket)
+        print(f"Client removed: {websocket.client}")
+
+
+def train_model_async(data):
+    model_version = data.get('version')
+    print("model_version:", model_version)
+
+    train_acc, val_acc, train_loss, val_loss = train_new_model(DATASET_FOLDER, model_version)
+
+    data = {
+        "version": f'ClamScanner_v{model_version}',
+        "train_accuracy": train_acc,
+        "validation_accuracy": val_acc,
+        "train_loss": train_loss,
+        "validation_loss": val_loss
+    }
+
+    return 'Trained Successfully'
+
+@app.post("/train/model")
+def train():
+    data = request.get_json()
+    threading.Thread(target=train_model_async, args=(data,)).start()
+    return jsonify({"status": "Training started"}), 200
+
+
+@app.post("/image/scan")
+def scan():
+    try:
+        file = predict.load_validate_image_file(request, jsonify)
+
+        filename = secure_filename(file.filename)
+        file_path = os.path.join('./', filename)
+        file.save(file_path)
+        
+        mollusk_classified_result = predict.mollusk_predict(file_path)
+        os.remove(file_path)
+        
+        return jsonify({"mollusk_classified_result": mollusk_classified_result}), 200
+
+    except Exception as e:
+        print("Error during image processing:", e)
+        return jsonify({"error": "Image processing failed"}), 500
 
 
 def run_flask():
