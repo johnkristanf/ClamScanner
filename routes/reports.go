5ef09f64ab97ae6ea3bbb2599f60e8f21aac4b24
John Kristan <jkpogz09@gmail.com>
2024-06-28 15:07:33 +0800

    implemented the route for fetching reports by month and moll usk

--- routes/reports.go
+++ routes/reports.go
@@ -16,11 +16,13 @@ func ReportsRoutes(router *http.ServeMux, reportsHandler *handlers.ReportHandler
 	router.HandleFunc("POST /insert/report", ParseHTTPHandler(reportsHandler.InsertReportHandler))
 
 	router.HandleFunc("GET /fetch/reports", ParseHTTPHandler(reportsHandler.FetchAllReportsHandler))
+	router.HandleFunc("GET /fetch/map/reports/{month}/{mollusk}", ParseHTTPHandler(reportsHandler.FetchMapReportsHandler))
+	
 	router.HandleFunc("GET /fetch/reports/city", adminAuth(ParseHTTPHandler(reportsHandler.FetchYearlyReportByCityHandler)))
 	router.HandleFunc("GET /fetch/reports/province", adminAuth(ParseHTTPHandler(reportsHandler.FetchYearlyReportByProvinceHandler)))
 	router.HandleFunc("GET /fetch/reports/mollusk", adminAuth(ParseHTTPHandler(reportsHandler.FetchReportPerMollusk)))
 
 	router.HandleFunc("PUT /update/report/status/{report_id}", ParseHTTPHandler(reportsHandler.UpdateReportStatusHandler))
 	router.HandleFunc("DELETE /delete/reports/{report_id}", adminAuth(ParseHTTPHandler(reportsHandler.DeleteReportHandler)))
-
+	
 }
