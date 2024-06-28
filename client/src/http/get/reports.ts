868e77964a364957cf140c57e4e8e62a00e8e69f
John Kristan <jkpogz09@gmail.com>
2024-06-28 15:05:02 +0800

    make the reports filter axios request

--- client/src/http/get/reports.ts
+++ client/src/http/get/reports.ts
@@ -1,8 +1,21 @@
 import axios, { AxiosResponse } from "axios";
+import { FetchMapReportsParams } from "../../types/map";
 
 export function FetchReports(): Promise<AxiosResponse<any, any>> {
     try {
         return axios.get("http://localhost:8080/fetch/reports", {
+            withCredentials: true
+        });
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error); 
+    }
+}
+
+
+export function FetchMapReports({ month, mollusk }: FetchMapReportsParams): Promise<AxiosResponse<any, any>> {
+    try {
+        return axios.get(`http://localhost:8080/fetch/map/reports/${month}/${encodeURIComponent(mollusk)}`, {
             withCredentials: true
         });
     } catch (error) {
