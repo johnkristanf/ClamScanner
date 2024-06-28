557b5b1eba2774896b0bddda3ae40f7d2bcc09a0
John Kristan <jkpogz09@gmail.com>
2024-06-28 15:05:33 +0800

    added the map component props type

---
+++ client/src/types/map.ts
@@ -0,0 +1,15 @@
+export interface SetViewOnClickProps {
+    MapCoor: [number, number];
+}
+
+export interface MapProps {
+    setMapCoor: React.Dispatch<React.SetStateAction<[number, number]>>;
+    MapCoor: [number, number];
+    setOpenReportsModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+  
+
+export interface FetchMapReportsParams {
+    month: string;
+    mollusk: string;
+}
