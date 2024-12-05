afdbc38fa3e9717255f1ed4b1a84fe7748b0e2ec
root <root@LAPTOP-2EHTO9SO>
2024-12-06 04:10:42 +0800

    feat: https request uri

--- client/src/http/post/train.ts
+++ client/src/http/post/train.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from "axios";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function TrainModel(version: string): Promise<AxiosResponse<any, any>> {
     try {
-        return axios.post("https://clamscanner.com/go/train/model", { version: version });
+        return axios.post("https://clamscanner.com/py/train/model", { version: version });
     } catch (error) {
         console.error(error);
         return Promise.reject(error); 
