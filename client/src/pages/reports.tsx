a44b7daa7acbda8e20a65de5418276dc8d363706
John Kristan Torremocha <johnkristan01@gmail.com>
2025-02-04 17:26:24 +0800

    feat: secure websocket uri

--- client/src/pages/reports.tsx
+++ client/src/pages/reports.tsx
@@ -12,7 +12,10 @@ const InitializeWSConnection = (setReports: React.Dispatch<React.SetStateAction<
     let ws: WebSocket | null = null;
 
     const connect = () => {
-        ws = new WebSocket("wss://clamscanner.com/go/ws/conn");
+        const productionWSurl = 'wss://clamscanner.com/go/ws/conn';
+        // const developmentWSurl = 'ws://localhost:5000/ws/conn';
+
+        ws = new WebSocket(productionWSurl);
 
         ws.onopen = () => {
             console.log("Golang WebSocket Connected");
